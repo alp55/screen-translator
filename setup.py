e2cf0b963b795fb2887c46acb5dc27736317fac2
Alperen Ulutaş <alperen_ulutas_29@hotmail.com>
2025-03-03 16:04:56 +0300

    Change

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from cx_Freeze import setup, Executable
 setup(
     name="screen_translator",
     version="1.0",
-    description="PySide6 ile yazılmış uygulama",
+    description="Screen Translator",
     executables=[Executable("main.py", base="Win32GUI")]
 )
